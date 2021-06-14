347030f9ee6828488cad5800d69c6a1bb6575e00
Brandon Akers <brandonakers729@gmail.com>
2021-06-13 21:05:57 -0500

    final commit

--- assets/README.md
+++ assets/README.md
@@ -1,23 +1,26 @@
 # README Generator
 ![Github License](https://img.shields.io/badge/License-MIT-yellow.svg)
 ## Description
-Generates a README markdown file.
+This apllication is used to generate a comprehensive README markdown file for your project.
 ## Table of Contents
 * [Installation](#Installation)
 * [Usage](#Usage)
 * [License](#License)
 * [Contributing](#Contributing)
 * [Tests](#Tests)
+* [Known Bugs](#bug)
 * [Questions](#Questions)
 ## Installation
-To install the README Generator, run 'npm i inquirer' in the command line. To invoke the prompts, run 'node index' in the command line.
+To install the README Generator, run 'npm i inquirer' in the command line. To invoke the prompts, run 'node index.js' in the command line.
 ## Usage
-Enter information as prompted.
+Follow along with the prompts in the terminal as requested.
 ## License
 MIT
 ## Contributing
-This is an open-source package that anyone can contribute to.
+Anyone can contribute to this code.
 ## Tests
-To run tests, install 'npm test' in the command line.
+No Tests.
+## Known Bugs
+No known bugs.
 ## Questions
 For additional information please contact me via GitHub at [https://github.com/codeb-a](https://github.com/codeb-a) or via email at [brandonakers729@gmail.com](mailto:brandonakers729@gmail.com?subject=[GitHub]%README%Generator).
--- index.js
+++ index.js
@@ -1,31 +1,34 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generateMD = (responses) => {
-    const license = responses.license === "none" ? "" : responses.license;
-    const licenseBadge = responses.license === "none" ? null : `![Github License](https://img.shields.io/badge/License-${license}-yellow.svg)`
-    return `# ${responses.title}
+const generateReadMe = (answers) => {
+    const license = answers.license === "none" ? "" : answers.license;
+    const licenseBadge = answers.license === "none" ? null : `![Github License](https://img.shields.io/badge/License-${license}-yellow.svg)`
+    return `# ${answers.title}
 ${licenseBadge}
 ## Description
-${responses.description}
+${answers.description}
 ## Table of Contents
 * [Installation](#Installation)
 * [Usage](#Usage)
 * [License](#License)
 * [Contributing](#Contributing)
 * [Tests](#Tests)
+* [Known Bugs](#bug)
 * [Questions](#Questions)
 ## Installation
-${responses.installation}
+${answers.installation}
 ## Usage
-${responses.usage}
+${answers.usage}
 ## License
-${responses.license}
+${answers.license}
 ## Contributing
-${responses.contributing}
+${answers.contributing}
 ## Tests
-${responses.tests}
+${answers.tests}
+## Known Bugs
+${answers.bug}
 ## Questions
-For additional information please contact me via GitHub at [https://github.com/${responses.github}](https://github.com/${responses.github}) or via email at [${responses.email}](mailto:${responses.email}?subject=[GitHub]%README%Generator).`
+For additional information please contact me via GitHub at [https://github.com/${answers.github}](https://github.com/${answers.github}) or via email at [${answers.email}](mailto:${answers.email}?subject=[GitHub]%README%Generator).`
 };
 
 inquirer
@@ -38,17 +41,17 @@ inquirer
         {
             type: 'input',
             name: 'description',
-            message: 'Provide a short description explaining the what, why, and how of your project.'
+            message: 'Describe your project here.'
         },
         {
             type: 'input',
             name: 'installation',
-            message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.'
+            message: 'Enter the steps to install your product here.'
         },
         {
             type: 'input',
             name: 'usage',
-            message: 'Provide instructions and examples for use. Include screenshots as needed.'
+            message: 'Input instructions on how to use your application.'
         },
         {
             type: 'list',
@@ -59,26 +62,31 @@ inquirer
         {
             type: 'input',
             name: 'contributing',
-            message: 'If you would like other developers to contribute to your application or package, include guidelines here.'
+            message: 'Enter in whether or not users can contribute to your project.'
         },
         {
             type: 'input',
             name: 'tests',
-            message: 'Describe how to run the tests on your application.'
+            message: 'Describe your tests.'
+        },
+        {
+            type: 'input',
+            name: 'bug',
+            message: 'Enter any bugs in your application.'
         },
         {
             type: 'input',
             name: 'github',
-            message: 'Enter your GitHub user name.'
+            message: 'Please enter your GitHub user name.'
         },
         {
             type: 'input',
             name: 'email',
-            message: 'Enter your email address.'
+            message: 'Please enter your email address.'
         },
     ])
-    .then((responses) => {
-        console.log(responses);
-        const readmeContent = generateMD(responses)
-        fs.writeFile('./utils/generated-README.md', readmeContent, (err) => err ? console.log(err) : console.log('README generated'));
+    .then((answers) => {
+        console.log(answers);
+        const readmeMdPage = generateReadMe(answers)
+        fs.writeFile('./assets/README.md', readmeMdPage, (err) => err ? console.log(err) : console.log('Successfully Generated'));
     });
